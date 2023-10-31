ab92d05e0bc4c1e30b0abfc285775ed905c621d1
Philip <phil658c@stud.kea.dk>
2023-10-31 14:30:22 +0100

    removed images and about section

--- js/aboutDescription.js
+++
@@ -1,9 +0,0 @@
-function toggleDescription(event, descriptionNumber) {
-    event.preventDefault(); 
-    var descriptionElement = document.getElementById("description" + descriptionNumber);
-    if (descriptionElement.style.display === "none") {
-        descriptionElement.style.display = "block";
-    } else {
-        descriptionElement.style.display = "none";
-    }
-}
